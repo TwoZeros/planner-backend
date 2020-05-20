88871516e17f53b8ab0391d4e42b12447bef54c2
Kirill Pakhtusov <pak.kiruha@yandex.ru>
2020-05-21 01:22:12 +0300

    fix timezone

--- Planner.Data/Repositories/CommentRepository.cs
+++ Planner.Data/Repositories/CommentRepository.cs
@@ -49,7 +49,7 @@ namespace Planner.Data.Repositories
         }
         public int GetRatingByDate(DateTime date, int clientId)
         {
-            var comments = _context.Comments.Where(p => p.CreateDate.Date == date.AddHours(-3).Date)
+            var comments = _context.Comments.Where(p => p.CreateDate.AddHours(3).Date == date.Date)
                 .Where(p=> p.ClientId==clientId).ToList();
             if (comments.Count == 0)
             {
